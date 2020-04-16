1883f72a7d896b3c58dad89358107e8065966428
Bundar <birnie.nick@gmail.com>
2020-04-15 20:42:22 -0400

    Scraper.py: scraper now working on first level. Successfully populated basic data for top 1000 Boulderers.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,31 +2,32 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
 import csv
+import time
 
 driver = webdriver.Chrome("/usr/lib/chromium/chromedriver")
 driver.get("https://www.8a.nu/scorecard/ranking/")
-content = driver.page_source
-soup = BeautifulSoup(content, features="lxml")
 
-if soup == None:
-    print("Soup not working right...")
+i = 0
+startI = 7
+while(i < 10):
+    time.sleep(5)
+    print("i: " + str(i))
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="lxml")
+    table = soup.find(id="ctl00_ContentPlaceholder_GridViewBoulder")
+    
 
-table = soup.find(id="ctl00_ContentPlaceholder_GridViewBoulder")
-print("tableType: " + str(type(table)))
-#print(str(table))
+    if table == None:
+        print(str(i) + ": Couldnt find the table... ")
+        print(table)
 
-if table == None:
-    print("Couldnt find the table... ")
-
-
-i=1
-csvfile = open('data/boulder-data/boulderData'+i+'.csv', 'w', newline='') 
-writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-writer.writerow(['Placing', 'Score', 'Name', 'DoB', 'Country'])
-
-for row in table.findAll("tr"):
-	cells = row.findAll("td")
-	if len(cells) == 8:
+    if i >= startI:
+        csvfile = open('data/boulder-data/boulderData'+str(i)+'.csv', 'w', newline='') 
+        writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Placing', 'Score', 'Name', 'DoB', 'Country'])
+        for row in table.findAll("tr"):
+            cells = row.findAll("td")
+            if len(cells) == 8:
                 placing = cells[0].find(text=True)
                 score = cells[2].find(text=True)
                 name = cells[4].find(text=True)
@@ -35,4 +36,13 @@ for row in table.findAll("tr"):
                 writer.writerow([placing, score, name, dob, country])
                 if type(dob) != type(1):
                     dob = 0
-                print(placing +": "+ name + ", " + score + ". Age: " + str(2020 - int(dob)) + " , Country: " + country)
+        csvfile.close()
+    
+    if i > 0:
+        driver.find_element_by_xpath('/html/body/form/div[3]/table/tbody/tr[2]/td[2]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/table/tbody/tr[101]/td/table/tbody/tr/td[2]/a').click()
+    else:
+        driver.find_element_by_xpath('/html/body/form/div[3]/table/tbody/tr[2]/td[2]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/table/tbody/tr[101]/td/table/tbody/tr/td/a').click()
+    
+    i = i + 1
+    driver.refresh()
+    time.sleep(3)
